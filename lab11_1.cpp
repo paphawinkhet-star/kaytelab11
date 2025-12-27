255854186e670d57e597eedb1fea09890fc8df46
paphawinkhet-star <paphawin.khet@gmail.com>
2025-12-27 13:50:59 +0700

    complete lab11_1 lab11_2

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,21 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int size = sizeof(grades) / sizeof(grades[0]);
+    srand(time(0));
+    int randomIndex = rand() % size;
+    cout << endl;
+    cout << "You will get " << grades[randomIndex]
+         << " in this 261102." << endl;
+    return 0;
+}
